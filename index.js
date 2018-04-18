b1e6955f7e9aac49afb9e23f781090009a2bcb5b
Jimmy Chan <skylinezum@gmail.com>
2018-04-18 02:46:07 -0700

    oauth2 template

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-// We can roll up all our behaviors in an App.
+// You'll want to set these with either `CLIENT_ID=abc zapier test` or `zapier env 1.0.0 CLIENT_ID abc`
+process.env.BASE_URL = process.env.BASE_URL || 'https://auth-json-server.zapier.ninja';
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1234';
+process.env.CLIENT_SECRET = process.env.CLIENT_SECRET || 'asdf';
+
+const authentication = require('./authentication');
+
+// To include the Authorization header on all outbound requests, simply define a function here.
+// It runs runs before each request is sent out, allowing you to make tweaks to the request in a centralized spot
+const includeBearerToken = (request, z, bundle) => {
+  if (bundle.authData.access_token) {
+    request.headers.Authorization = `Bearer ${bundle.authData.access_token}`;
+  }
+  return request;
+};
+
 const App = {
   // This is just shorthand to reference the installed dependencies you have. Zapier will
   // need to know these before we can upload
   version: require('./package.json').version,
   platformVersion: require('zapier-platform-core').version,
 
-  // beforeRequest & afterResponse are optional hooks into the provided HTTP client
+  authentication: authentication,
+
   beforeRequest: [
+    includeBearerToken
   ],
 
   afterResponse: [
   ],
 
-  // If you want to define optional resources to simplify creation of triggers, searches, creates - do that here!
   resources: {
   },
 
